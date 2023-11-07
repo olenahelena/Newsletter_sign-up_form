a4263a220eaafe4035175029a37b1800051d2c20
Olena Shevchenko <lemonslice67@gmail.com>
2023-11-07 11:59:19 +0100

    changed project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myButton = document.getElementById('submit__btn');
+const dismissButton = document.getElementById('dismiss__btn');
 
 const emailInput = document.getElementById('email');
 const mainContainer = document.getElementById('page-main');
@@ -12,10 +13,12 @@ myButton.onclick = function () {
   if (emailValue === '' || !isValidEmail(emailValue)) {
     errorText.textContent = 'Valid email required';
     emailInput.classList.add('input_border_error');
+    emailInput.style.color = 'red';
   } else {
     errorText.textContent = ''; // Clear the error message if the email is valid
     mainContainer.classList.add('error');
     successMessage.classList.remove('error');
+     emailInput.classList.remove('input_border_error');
 
   }
 };
@@ -24,3 +27,9 @@ function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+dismissButton.onclick = function () {
+  mainContainer.classList.remove('error');
+  successMessage.classList.add('error');
+  emailInput.value = '';
+};
